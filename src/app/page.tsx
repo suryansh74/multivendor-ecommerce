4b3993ad633bb8e8ee4d21e8d970bbe4fba99364
suryansh74 <suryanshawasthi56@gmail.com>
2025-06-12 16:23:16 +0530

    font and all shadcn component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Roboto_Flex } from "next/font/google";
+
+const bokerFont = Roboto_Flex({
+  subsets: ['latin'],
+  weight: ['400'],
+  display: 'swap'
+})
+
 export default function Home() {
   return (
-    <div className="font-bold text-3xl text-blue-500">
-      Hello World
+    <div className={`text-lg ${bokerFont.className}`}>
+      <p className="">hello world</p>
+      <Button variant={"default"}>Click Me</Button>
     </div>
   );
 }
